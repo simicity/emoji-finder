7b17c92ef0caa155179264d573ac951d8d91d309
simicity <simi40nm@gmail.com>
2023-02-12 11:41:16 -0800

    Add emoji finder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import API_KEY from './config';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchGroup, setSearchGroup] = useState({
+    groups: [
+      {id: "all", name: "All"},
+      {id: "smileys_emotion", name: "Faces"},
+      {id: "people_body", name: "People"},
+      {id: "component", name: "Component"},
+      {id: "animals_nature", name: "Animal & Nature"},
+      {id: "food_drink", name: "Food & Drink"},
+      {id: "travel_places", name: "Travel"},
+      {id: "activities", name: "Activities"},
+      {id: "objects", name: "Objects"},
+      {id: "symbols", name: "Symbols"},
+      {id: "flags", name: "Flags"},
+    ],
+    selected: "all"
+  });
+  const [searchResults, setSearchResults] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(
+          `https://api.api-ninjas.com/v1/emoji?${searchTerm.length > 0 ? "name=" + searchTerm : ""}${searchTerm.length > 0 && searchGroup.selected !== "all" ? "&" : ""}${searchGroup.selected !== "all" ? "group=" + searchGroup.selected : ""}`,
+          {
+            headers: {
+              'X-Api-Key': API_KEY
+            }
+          }
+        );
+        const data = await res.json();
+        setSearchResults(data);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    }
+    if (searchTerm.length > 0 || searchGroup.selected !== "all") {
+      fetchData();
+    }
+  }, [searchTerm, searchGroup.selected])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="mx-5 my-4">
+      <h1 className="d-flex justify-content-center m-4">Emoji Finder 👩🏻‍💻</h1>
+      <div className="d-flex justify-content-center mb-5">
+        <form style={{ width: "350px" }}>
+          <input
+            className="d-flex justify-content-center form-control"
+            type="text"
+            value={searchTerm}
+            placeholder="Search by keywords"
+            onChange={event => setSearchTerm(event.target.value)}
+          />
+        </form>
+        <select 
+          className="form-select mx-3"
+          style={{ width: "200px" }}
+          value={searchGroup.selected}
+          onChange={(event) => {
+            event.persist();
+            setSearchGroup((prev) => ({
+              ...prev,
+              selected: event.target.value
+            }));
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {searchGroup.groups.map((item) => (
+            <option key={item.id} value={item.id}>{item.name}</option>
+          ))}
+        </select>
+      </div>
+      {error && 
+        <div className="d-flex justify-content-center">{error.message}</div>
+      }
+      {isLoading
+      ? <div className="d-flex justify-content-center">Loading...</div>
+      : (
+          <div className="row row-cols-1 row-cols-md-5 g-4">
+            {searchResults.map(result => (
+              <div key={result.id} className="col">
+                <div className="card">
+                  <div className="d-flex justify-content-center card-body">
+                    {result.name} {result.character}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
     </div>
   );
 }
